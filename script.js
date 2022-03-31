70b1047e0787c3be009c9545c85b402e78a62a25
nichsieh <nihsieh@hotmail.com>
2022-03-30 22:01:25 -0400

    updates

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
-const cluePauseTime = 333; //how long to pause in between clues
+// const clueHoldTime = 1000; //how long to hold each clue's light/sound
+// const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0; 
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5;  //must be between 0.0 and 1.0
 var guessCounter = 0;
 
+function patternCreator(){
+  const pattern = [];
+  length = Math.floor(Math.random() * 10);
+  for (let i = 0; i < length; i++){
+    pattern[i] = Math.floor(Math.random() * 7);
+  }
+  return pattern
+}
+
+var pattern = patternCreator();
+
 function startGame(){
   //initialize game variables
   progress = 0;
   gamePlaying = true;
-  
   // swap the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
-  
   playClueSequence();
 }
 
 function stopGame(){
   //initialize game variables
   gamePlaying = false;
-  
   // swap the Start and Stop buttons
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
@@ -37,7 +44,9 @@ const freqMap = {
   1: 261.6,
   2: 325,
   3: 392,
-  4: 419
+  4: 419,
+  5: 530,
+  6: 623
 }
 
 
@@ -86,22 +95,37 @@ function clearButton(btn){
 }
 
 function playSingleClue(btn){
-  if(gamePlaying){
-    lightButton(btn);
-    playTone(btn,clueHoldTime);
-    setTimeout(clearButton,clueHoldTime,btn);
+  if (progress == 0){
+    const clueHoldTime = 1000;
+    const cluePauseTime = 333;
+    if(gamePlaying){
+      lightButton(btn);
+      playTone(btn,clueHoldTime);
+      setTimeout(clearButton,clueHoldTime,btn);
+    }
+  } else {
+    const clueHoldTime = 1000 / progress + 1;
+    const cluePauseTime = 333 / progress + 1;
+    if(gamePlaying){
+      lightButton(btn);
+      playTone(btn,clueHoldTime);
+      setTimeout(clearButton,clueHoldTime,btn);
+    }
+    
   }
 }
 
 function playClueSequence(){
+  const clueHoldTime = 1000;
+  const cluePauseTime = 333;
   guessCounter = 0;
   context.resume()
   let delay = nextClueWaitTime; //set delay to initial wait time
   for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
     setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
-    delay += clueHoldTime 
-    delay += cluePauseTime;
+    delay += clueHoldTime/progress+1
+    delay += cluePauseTime/progress+1;
   }
 }
 
